4facc3fed969d8d105eae64923fecb021fd81a65
Huzaifa-Khan12 <119830096+Huzaifa-Khan12@users.noreply.github.com>
2025-03-03 08:58:59 +0500

    Set up Database Connection
    Established MongoDB connection and also divided the routes into their own controller using custom middleware

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const login = (req, res) => {
+  res.send("Login Route");
+};
+
+export const logout = (req, res) => {
+  res.send("Logout Route");
+};
+
+export const signup = (req, res) => {
+  res.send("SignUp Route");
+};
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+//middleware to go the auth routes
+app.use("/api/auth", authRoutes);
+
 app.listen(port, () => {
+  connectToMongoDB();
   console.log(`Server listening on port: ${port}`);
 });
