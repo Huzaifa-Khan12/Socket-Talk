d969e557d85afcf9f1837c9d02689c75f981e478
Huzaifa-Khan12 <119830096+Huzaifa-Khan12@users.noreply.github.com>
2025-03-06 16:30:04 +0500

    Add Get User Endpoint
    Created the Get Users Endpoint which fetches all the users to be displayed in the sidebar to chat with

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,17 @@
+import User from "../models/user.model.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+
+    //fetch all users from db except current user itself
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.log("Error in getUsersForSidebar", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 dotenv.config();
@@ -14,8 +17,10 @@ app.use(express.json());
 app.use(cookieParser()); //to get the cookie parsed for the files after (especially in protectRoute.js)
 //middleware to go the auth routes
 app.use("/api/auth", authRoutes);
-//middleware to go the auth routes
+//middleware to go the message routes
 app.use("/api/messages", messageRoutes);
+//middleware to go the user routes
+app.use("/api/users", userRoutes);
 
 // app.get("/", (req, res) => {
 //   res.send("Hello World!");
