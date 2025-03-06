4ca0af7c16e82b6d23cf7156a8639fb0b0715c26
Huzaifa-Khan12 <119830096+Huzaifa-Khan12@users.noreply.github.com>
2025-03-06 16:08:49 +0500

    Add Get Messages Endpoint
    Created Get Messages Endpoint, which is to get the messages between current logged in user and other users he have chatted with

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -5,7 +5,6 @@ export const sendMessage = async (req, res) => {
   try {
     const { message } = req.body;
     const recieverId = await req.params.id;
-    console.log(recieverId);
 
     //get the _id from user set in the protectRoute middleware
     const senderId = req.user._id;
@@ -38,6 +37,27 @@ export const sendMessage = async (req, res) => {
     await Promise.all([conversation.save(), newMessage.save()]);
 
     res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in send message controller", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    //userToChatId is the user id whom we are sending messages to
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages");
+    //populate method gets the messages references from Message model and populates the actual messages in the Conversation records
+    //So instead of getting Array of Message Ids, we get Array of Objects containg the message records
+
+    if (!conversation) res.status(200).json([]); //send an empty array is conversation does not exist
+
+    res.status(200).json(conversation.messages);
   } catch (error) {
     console.log("Error in send message controller", error);
     res.status(500).json({ error: "Internal server error" });
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { sendMessage } from "../controllers/message.controller.js";
+import { sendMessage, getMessages } from "../controllers/message.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
+//to get messages between 2 users
+router.get("/:id", protectRoute, getMessages);
 //:id is the recieverId that the sender is sending message to
 router.post("/send/:id", protectRoute, sendMessage);
 
