548b5e52face9835fe77a36f5ff4673e88537716
Huzaifa-Khan12 <119830096+Huzaifa-Khan12@users.noreply.github.com>
2025-03-09 16:23:47 +0500

    Set React Routes
    Set up the routes using react router

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import GenderCheckbox from "./GenderCheckbox";
+import { Link } from "react-router";
 
 const SignUp = () => {
   return (
@@ -57,12 +58,12 @@ const SignUp = () => {
 
           <GenderCheckbox />
 
-          <a
-            href="#"
+          <Link
+            to="/login"
             className="text-sm  text-gray-200 hover:underline hover:text-blue-600 mt-2 inline-block"
           >
             Already have an account?
-          </a>
+          </Link>
 
           <div>
             <button className="btn btn-block btn-sm mt-2">Sign Up</button>
