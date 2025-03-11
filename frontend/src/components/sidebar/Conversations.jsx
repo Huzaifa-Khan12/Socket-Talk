5389e4a542cc6db418edf1b82e1b24a942e69017
Huzaifa-Khan12 <119830096+Huzaifa-Khan12@users.noreply.github.com>
2025-03-11 16:29:14 +0500

    Implement custom useGetConversations hook to fetch and display users in sidebar
    Also added the details to their respected components, i.e, the Coversation and Conversations components

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import Conversation from "./Conversation";
+import useGetConversations from "../../hooks/useGetConversations";
+import { getRandomEmoji } from "../../utils/emojis";
 
 const Conversations = () => {
+  const { loading, conversations } = useGetConversations();
+  console.log("Conversatios: ", conversations);
+  const idx = conversations.length - 1;
   return (
     <div className="py-2 flex flex-col overflow-auto">
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
+      {conversations.map((conversation) => (
+        <Conversation
+          key={conversation._id}
+          conversation={conversation}
+          emoji={getRandomEmoji()}
+          lastIdx={idx}
+        />
+      ))}
+
+      {loading ? (
+        <span className="loading loading-spinner mx-auto"></span>
+      ) : null}
     </div>
   );
 };
---
+++ frontend/src/hooks/useGetConversations.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+const useGetConversations = () => {
+  const [loading, setLoading] = useState(false);
+  const [conversations, setCoversations] = useState([]);
+
+  //using it only once
+  useEffect(() => {
+    const getConversations = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch("/api/users");
+        const data = await res.json();
+        if (data.error) {
+          throw new Error(data.error);
+        }
+        setCoversations(data);
+      } catch (error) {
+        toast.error(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getConversations();
+  }, []); //empty dependency array
+
+  return { loading, conversations };
+};
+
+export default useGetConversations;
