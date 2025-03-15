f35fb94906be38bdd193522222d8accf5e476b55
Huzaifa-Khan12 <119830096+Huzaifa-Khan12@users.noreply.github.com>
2025-03-15 16:59:20 +0500

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -13,6 +14,8 @@ dotenv.config();
 
 const port = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser()); //to get the cookie parsed for the files after (especially in protectRoute.js)
 //middleware to go the auth routes
@@ -22,9 +25,11 @@ app.use("/api/messages", messageRoutes);
 //middleware to go the user routes
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(port, () => {
   connectToMongoDB();
